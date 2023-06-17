835eee64ff75b72f99a5d8099ac426540a99e5f0
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-06-17 17:54:19 +0530

    navbar effect done

--- uhackathon23/src/Components/Navbar/Navbar.jsx
+++ uhackathon23/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React,{useEffect,useRef, useState} from 'react'
 import upesLogo from '../Images/uhacklogo1.png'
+import upesWhiteLogo from '../Images/uhacklogowhite1.png'
 import './Navbar.css'
 const Navbar = () => {
+  const [img,setImg]=useState(upesWhiteLogo);
+  const [color,setColor]=useState("white");
+  const [background, setBackground] = React.useState("transparent");
+  const handleScroll = () => {
+    const navbar=document.body.scrollTop
+      if(navbar!==0){
+        setBackground("white")
+      setImg(upesLogo)
+      setColor("black")
+      }
+     else {
+      setColor("white")
+      setImg(upesWhiteLogo)
+      setBackground("transparent")
+    }
+  };
+
+  useEffect(() => {
+   document.body.addEventListener("scroll", handleScroll);
+  });
   return (
-    <nav className="navbar navbar-expand-lg bg-light ">
+    <nav className="navbar fixed-top navbar-expand-lg " style={{backgroundColor:background}}>
   <div className="container">
     <a className="navbar-brand" href="/">
-    <img src={upesLogo} alt="Bootstrap"/>
+    <img src={img} alt="Bootstrap"/>
     </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="/">Home</a>
-        <a className="nav-link" href="/">About</a>
-        <a className="nav-link" href="/">Gallery</a>
-        <a className="nav-link" href="/">Contact</a>
+      <div className="navbar-nav" >
+        <a className="nav-link active" aria-current="page" href="/" style={{color:color}}>Home</a>
+        <a className="nav-link" href="/" style={{color:color}}>About</a>
+        <a className="nav-link" href="/" style={{color:color}}>Gallery</a>
+        <a className="nav-link" href="/" style={{color:color}}>Contact</a>
         <button className='nav-link join-btn'>Join Us</button>
       </div>
     </div>
