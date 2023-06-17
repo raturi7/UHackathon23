83e42a1822c13180070aa71945fcee18973feafd
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-06-17 17:08:29 +0530

    routes added

---
+++ uhackathon23/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='flex justify-center'>
+        <h1>Get in Touch</h1>
+    </div>
+    
+  )
+}
+
+export default Contact
