d0dc4ffee52c10b084cbf9b8eb35993d5bdb32b0
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-06-17 11:19:26 +0530

    timmer added motion added

---
+++ uhackathon23/src/Components/AnimatedText.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "./Hero/Hero.css";
+const quote = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.08,
+    },
+  },
+};
+const singleWord = {
+  initial: {
+    opacity: 0,
+y:50,
+
+  },
+  animate: {
+    opacity: 1,
+    y:0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const AnimatedText = ({ text, className = "" }) => {
+  return (
+    <div className="">
+      <motion.h5
+        className={`    ${className}`}
+        variants={quote} // corrected prop name to "variants"
+        initial="initial"
+        animate="animate"
+      >
+        {text.split(" ").map((word, index) => (
+          <motion.span key={word + "-" + index} className="inline-block" variants={singleWord}
+
+          >
+            {word}&nbsp;
+          </motion.span>
+        ))}
+      </motion.h5>
+    </div>
+  );
+};
+
+export default AnimatedText;
