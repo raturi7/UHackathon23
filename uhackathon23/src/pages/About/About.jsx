397ebba568c3280b36a3a0babca26a3cbab2d3c7
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-06-18 12:26:44 +0530

    Revert "about page"
    This reverts commit da52630b3b18a60d6988aae8e205ca748050c46e.

--- uhackathon23/src/pages/About/About.jsx
+++ uhackathon23/src/pages/About/About.jsx
@@ -1,50 +1,9 @@
-// import React from 'react'
-
-// const About = () => {
-//   return (
-//     <div>About</div>
-//   )
-// }
-
-// export default About
-
 import React from 'react'
-import './About.css'
 
 const About = () => {
   return (
-    <>
-  <div className='container'>
-    <div className='heading'>
-      <h1>UHackathon 4.0 Meta</h1>
-    </div>
-    <section>
-      <p> After the success of UHackathon 1.0, 2.0 & 3.0 School of Computer Science is back with the 4th edition of UPES’ s Flagship event UHackathon 4.0META proposed to be held on 1st and 2nd September 2023. It’s a signature event of School Of Computer Science, listed in the Event Calendar. UHackathon 4.0 META is an initiative in sync with the vision of UPES, towards providing a mega platform to the programming community, technophiles and techpreneurs to address the critical challenges of the industry and society by building innovative solutions in form of prototypes leveraging technologies under the umbrella of Metaverse. A 24 hour coding marathon planning to invite participants from G20 Nations and our associate International Universities to come together and participate in developing solutions towards achieving UN SDG goals leveraging the emerging technologies and technologies of tomorrow i.e, Metaverse Technologies including AR (Augmented Reality), VR(Virtual Reality), Artificial Intelligence, IoT, Blockchain, Extended reality, 3D Modelling, Brain Computer Interface, Spatial / Edge computing. The broader areas of UHackathon 4.0 are focused, but not limited to the fields of, Disaster Management, Environmental Sustainability, EdTech solutions, Blockchain, Consumer Tech, Healthcare & Biomedical Devices, Retail, Women Welfare, Smart Communication Systems, Agriculture & Rural Development, Food Technology, Smart Vehicles, Robotics & Drones, and AI systems.</p>
-    </section>
-   
-    <div class="photos">
-  <div class='image1'>
-  <img src="/home/deepali/Documents/CSI/Uhackathon 2023/uhackathon23/src/Components/Images/uhacklogo1.png" alt="" width="533.33" height="538"/>
-  </div>
-  <div class='image2'>
-    <img src="glow.png" alt="" width="533.33" height="538"/>
-  </div>
-  
-</div>
-
-     <div className='secondheading'>
-      <h1>About UPES</h1></div>
-      <section id='aboutupes'>
-        <p>The University of Petroleum and Energy Studies (UPES) is a private NAAC A accredited university in the state of Uttrakhand, Dehradun. Established in 2003 through the UPES Act, 2003 of the State Legislature of Uttarakhand, UPES is a UGC-recognized university. UPES ranked 46th for School of Business, 25th for School of Law, and 91st for the School of Engineering by National Institutional Ranking Framework in Year 2021.Two programs of UPES B Tech Chemical Engineering & B Tech Automotive Design Engineering of School of Engineering are accredited by National Board of Accreditation. The University has received 5-Stars on Employability (placements), Academic Development Program Strength and campus facilities and 4-Stars for overall teaching by QS Rating. UPES offers graduate and postgraduate programs through its eight schools: School of Engineering, School of Computer Science, School of Design, School of Law, School of Business, School of Health Sciences, School of Modern Media and School of Liberal Studies. UPES has collaboration with top-ranking universities across the globe to provide the best education experience to our students. With more than 50 partner universities across the globe and 7 active associations with international bodies. UPES Council for Innovation and Entrepreneurship launched Runway Incubator, an initiative to help budding entrepreneurs get through the stages of setting a start-up through the confluence of right mentorship, finance, and legal support. UPES is the first Indian university to achieve this coveted certification of “Great Place to work” in 2019 and has continued to be so till present. </p>
-      </section>
-<div className='trigger'>
-      <button type="button">Know More</button>
-      </div>
-      </div>
-    
-           </> 
-           )
-
+    <div>About</div>
+  )
 }
 
 export default About
