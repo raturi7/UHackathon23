0ae56311ccc0d281e37ebabbabc6e417536a1722
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-06-23 23:38:00 +0530

    add button

--- uhackathon23/src/pages/About/About.jsx
+++ uhackathon23/src/pages/About/About.jsx
@@ -98,6 +98,9 @@ const About = () => {
           <section id='aboutupes'>
             <p>The picturesque Valley of Dehradun is the winter capital along with being the largest, as well as, the most populated city in Uttarakhand. It is the administrative headquarters of the eponymous district and is governed by the Dehradun Municipal Corporation. The city’s cosmopolitan aura amalgamated seamlessly with it’s unparalleled natural beauty. As per the legends, the great guru Drona of Mahabharata, lived here with his wife Kripi. Traditionally part of the Garhwal region, Dehradun lies along National Highway 7 at a distance of 248 km (154 mi) north of India's capital New Delhi and is well-connected through road, rail and air with rest of the country. With numerous premier schools and centers of higher education, Dehradun has emerged out as one of the prominent education hubs in India. Haridwar, the “Gateway to Gods” and Rishikesh, the "Yoga Capital of the World" are just an hour’s drive from Dehradun. Moreover, it is among the number of cities selected by Government of India, as part of its smart city project. It is the third largest city in the Himalayas after Kathmandu and Srinagar. Know more about Uttarakhand & Dehradun.</p>
           </section>
+          <div className='trigger d-flex justify-content-start mx-3'>
+            <a href='https://uttarakhandtourism.gov.in' className='footer-link p-2 mt-4' style={{textDecoration:"none"}}>Know More</a>
+          </div>
         </div>
         {images()}
       </div>
