3666e209d0dd2c03f42ca5378841a68e51d619bf
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-06-25 12:02:03 +0530

    ululi

--- uhackathon23/src/Components/Hero/Hero.jsx
+++ uhackathon23/src/Components/Hero/Hero.jsx
@@ -69,9 +69,9 @@ const Hero = () => {
   }, []);
 
   return (
-    <div>
+    <div className="small-mobile">
       <div
-        className="small-mobile container d-flex justify-content-center hero-container"
+        className=" container d-flex justify-content-center hero-container"
         style={{ color: "#E4E4E4", textAlign: "left", height: "85vh" }}
       >
         <div className="container">
