e32aa44228c72aa62e5b396dd276c3baf963ecf9
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-06-27 16:54:16 +0530

    new

--- uhackathon23/src/Components/Timeline/Timeline.jsx
+++ uhackathon23/src/Components/Timeline/Timeline.jsx
@@ -12,35 +12,29 @@ const Timeline = () => {
         <div className="col-md-12">
           <div className="main-timeline">
             <div className="timeline">
-              <div  className="timeline-content">
+              <div className="timeline-content">
                 <div className="timeline-year">27 June 2023</div>
                 <h3 className="title">Official Website Live</h3>
                 <p className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  Mark your calendars! The official website for our hackathon
+                  will go live on 7th June 2023, providing all the essential
+                  information, guidelines, and updates for participants. Get
+                  ready to embark on an exciting hacking journey!
                 </p>
               </div>
             </div>
-            <div className="timeline">
-              <a href="/" className="timeline-content">
-                <div className="timeline-year">29 June 2023</div>
-                <h3 className="title">Registrations Open</h3>
-                <p className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                </p>
-              </a>
-            </div>
+
             <div class="timeline">
               <a href="/" class="timeline-content">
                 <div class="timeline-year">29 June 2023</div>
                 <h3 class="title">Registrations Open</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  It's time to take the first step toward an incredible hacking
+                  experience. Registrations for our hackathon are now open! Take
+                  advantage of the opportunity to showcase your skills,
+                  collaborate with fellow innovators, and compete for exciting
+                  prizes. Sign up today and secure your spot in this thrilling
+                  event.
                 </p>
               </a>
             </div>
@@ -51,9 +45,11 @@ const Timeline = () => {
                 </div>
                 <h3 class="title">Theme Reveal</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  The moment you've been waiting for has arrived! We are
+                  thrilled to unveil the hackathon's theme. Let your creativity
+                  flow as you brainstorm innovative ideas within this exciting
+                  theme. Abstract submission officially begins, giving you a
+                  platform to showcase your unique approach to problem-solving.
                 </p>
               </a>
             </div>
@@ -62,9 +58,11 @@ const Timeline = () => {
                 <div class="timeline-year">11 July 2023</div>
                 <h3 class="title">Abstract Submission Starts</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  The anticipation is building! The results of the preliminary
+                  round of the hackathon will be announced soon. Stay tuned to
+                  find out if your team has reached the final stage. Prepare
+                  yourself for the ultimate showdown in the upcoming Uhackathon
+                  4.0 META finale.
                 </p>
               </a>
             </div>
@@ -73,9 +71,12 @@ const Timeline = () => {
                 <div class="timeline-year">31 July 2023</div>
                 <h3 class="title">Registrations & Abstract Submission Ends</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  Hurry! The clock is ticking. Make sure you complete your
+                  registration and submit your abstract before the deadline.
+                  Don't miss out on the chance to demonstrate your skills,
+                  tackle real-world challenges, and compete against top-notch
+                  talent from around the globe. Submit your entry to be
+                  considered for the preliminary round.
                 </p>
               </a>
             </div>
@@ -84,9 +85,11 @@ const Timeline = () => {
                 <div class="timeline-year">18 Aug. 2023</div>
                 <h3 class="title">Result Announcement</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  The anticipation is building! The results of the preliminary
+                  round of the hackathon will be announced soon. Stay tuned to
+                  find out if your team has reached the final stage. Prepare
+                  yourself for the ultimate showdown in the upcoming Uhackathon
+                  4.0 META finale.
                 </p>
               </a>
             </div>
@@ -97,9 +100,12 @@ const Timeline = () => {
                 </div>
                 <h3 class="title">UHackathon 4.0 Finale</h3>
                 <p class="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer males uada tellus lorem, et condimentum neque commodo
-                  Integer males uada tellus lorem, et condimentum neque commodo
+                  The grand finale of Uhackathon 4.0 META is here! Join us for
+                  an electrifying event where the most innovative teams showcase
+                  their cutting-edge solutions. Witness the brilliance and
+                  ingenuity of participants as they compete for the top spot.
+                  Get ready to celebrate the spirit of innovation and excellence
+                  in this thrilling finale.
                 </p>
               </a>
             </div>
