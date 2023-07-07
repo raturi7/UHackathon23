2a089798b11ca17f2170d8c5eeff7f51924bc775
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-07-07 21:57:13 +0530

    add ps pop up

--- uhackathon23/src/pages/Gallery/Gallery.jsx
+++ uhackathon23/src/pages/Gallery/Gallery.jsx
@@ -27,40 +27,40 @@ const Gallery = () => {
 
   const ref1 = useRef();
   const modal = useRef();
-  const [selectedImage, setSelectedImage] = useState(["","",""]); // Store the selected image
+  const [selectedImage, setSelectedImage] = useState(["", "", ""]); // Store the selected image
 
   let data = [
-    [img3," The Victorious Champions","Amidst thunderous applause, we celebrated the victorious champions, honoring their remarkable achievements and recognizing their relentless pursuit of excellence that set them apart as shining beacons of success."],
-    [img9,"The Exceptional Runner-Ups","Honoring the exceptional runner-ups, their talent and dedication inspired us all, leaving an indelible impact throughout the competition"],
-    [img5,"Commending The Impressive Second Runner-Ups","Commending the impressive second runner-ups, their outstanding performance and unwavering determination left an unforgettable mark, inspiring us with their remarkable journey "],
-    [img17,"The Winning Teams","Standing ovation to the winning teams, whose outstanding accomplishments and unwavering spirit of innovation have reshaped the landscape of success, leaving an indelible mark on this remarkable journey."],
-    [img13,"A Guiding Light of Knowledge","A guiding light of knowledge and motivation, gracing our occasion with their esteemed presence, igniting intellects and molding the course of inventive endeavors"],
-    [img6,"A beacon of wisdom","A beacon of wisdom and inspiration, illuminating our event with their presence, as they ignite minds and shape the path of innovation"],
-    [img7,"Token of Gratitude","With deep appreciation, we expressed our heartfelt gratitude and presented a token of gratitude to our esteemed chief guests, acknowledging their wisdom, guidance, and unwavering support that had illuminated our path and enriched our event."],
-    [img2,"The Educational Orchestrators","The educational orchestrators, guiding and nurturing young minds, fostering an environment of growth and inspiration, as they pave the way for tomorrow's leaders and innovators."],
-    [img19,"Igniting a Collective Spirit","An exuberant show of good faith and well wishes, igniting a collective spirit of innovation, collaboration, and boundless possibilities."],
-    [img12,"The Discerning Jury","The discerning jury, unraveling brilliance, shaping the destiny of innovation with insightful judgment."],
-    [img14,"Grace and Expression","A mesmerizing symphony of bodies, where rhythm and passion intertwine, painting the canvas of movement with grace and expression."],
-    [img10,"A band of digital warriors","The masterminds behind the scenes, orchestrating creativity and precision, weaving together a seamless tapestry of inspiration and success"],
-    [img8,"A passionate presenter","A passionate presenter, unveiling their project with unwavering enthusiasm, captivating the audience with their innovation and leaving a lasting impression of creativity in action"],
-    [img11,"A United Force","A united force of innovators, standing on the precipice of brilliance, as they present their revolutionary project, igniting minds and leaving an indelible mark on the realm of possibilities."],
-    [img15,"A Confident Participant","A confident participant, stepping into the spotlight, ready to showcase their project with fervor, leaving the audience inspired and amazed by their ingenuity and dedication."],
-    [img16,"A Dynamic Ensemble","A dynamic ensemble of passionate minds, converging from diverse horizons, ready to unleash their creativity and immerse themselves in the exhilarating journey of discovery and collaboration"],
-    [img4,"Immersed in the Event's Energy","A dynamic gathering of participants, immersed in the event's energy, exchanging ideas, showcasing talents, and fueling an atmosphere of innovation"],
-    [img18,"Brimming with Excitement","A participant brimming with excitement, radiating enthusiasm and anticipation, eagerly diving into the event's vibrant atmosphere, ready to embark on an unforgettable journey of creativity and discovery"],
-    [img1,"A Visionary Squad","A visionary squad, poised to unveil their groundbreaking creation, as they captivate the judges with their ingenuity and showcase the future in their hands."],
-    [img20,"A Band of Digital Warriors","A band of digital warriors, forging innovation through lines of code, hacking through challenges to unlock a future of endless possibilities"]
+    [img3, " The Victorious Champions", "Amidst thunderous applause, we celebrated the victorious champions, honoring their remarkable achievements and recognizing their relentless pursuit of excellence that set them apart as shining beacons of success."],
+    [img9, "The Exceptional Runner-Ups", "Honoring the exceptional runner-ups, their talent and dedication inspired us all, leaving an indelible impact throughout the competition"],
+    [img5, "Commending The Impressive Second Runner-Ups", "Commending the impressive second runner-ups, their outstanding performance and unwavering determination left an unforgettable mark, inspiring us with their remarkable journey "],
+    [img17, "The Winning Teams", "Standing ovation to the winning teams, whose outstanding accomplishments and unwavering spirit of innovation have reshaped the landscape of success, leaving an indelible mark on this remarkable journey."],
+    [img13, "A Guiding Light of Knowledge", "A guiding light of knowledge and motivation, gracing our occasion with their esteemed presence, igniting intellects and molding the course of inventive endeavors"],
+    [img6, "A beacon of wisdom", "A beacon of wisdom and inspiration, illuminating our event with their presence, as they ignite minds and shape the path of innovation"],
+    [img7, "Token of Gratitude", "With deep appreciation, we expressed our heartfelt gratitude and presented a token of gratitude to our esteemed chief guests, acknowledging their wisdom, guidance, and unwavering support that had illuminated our path and enriched our event."],
+    [img2, "The Educational Orchestrators", "The educational orchestrators, guiding and nurturing young minds, fostering an environment of growth and inspiration, as they pave the way for tomorrow's leaders and innovators."],
+    [img19, "Igniting a Collective Spirit", "An exuberant show of good faith and well wishes, igniting a collective spirit of innovation, collaboration, and boundless possibilities."],
+    [img12, "The Discerning Jury", "The discerning jury, unraveling brilliance, shaping the destiny of innovation with insightful judgment."],
+    [img14, "Grace and Expression", "A mesmerizing symphony of bodies, where rhythm and passion intertwine, painting the canvas of movement with grace and expression."],
+    [img10, "A band of digital warriors", "The masterminds behind the scenes, orchestrating creativity and precision, weaving together a seamless tapestry of inspiration and success"],
+    [img8, "A passionate presenter", "A passionate presenter, unveiling their project with unwavering enthusiasm, captivating the audience with their innovation and leaving a lasting impression of creativity in action"],
+    [img11, "A United Force", "A united force of innovators, standing on the precipice of brilliance, as they present their revolutionary project, igniting minds and leaving an indelible mark on the realm of possibilities."],
+    [img15, "A Confident Participant", "A confident participant, stepping into the spotlight, ready to showcase their project with fervor, leaving the audience inspired and amazed by their ingenuity and dedication."],
+    [img16, "A Dynamic Ensemble", "A dynamic ensemble of passionate minds, converging from diverse horizons, ready to unleash their creativity and immerse themselves in the exhilarating journey of discovery and collaboration"],
+    [img4, "Immersed in the Event's Energy", "A dynamic gathering of participants, immersed in the event's energy, exchanging ideas, showcasing talents, and fueling an atmosphere of innovation"],
+    [img18, "Brimming with Excitement", "A participant brimming with excitement, radiating enthusiasm and anticipation, eagerly diving into the event's vibrant atmosphere, ready to embark on an unforgettable journey of creativity and discovery"],
+    [img1, "A Visionary Squad", "A visionary squad, poised to unveil their groundbreaking creation, as they captivate the judges with their ingenuity and showcase the future in their hands."],
+    [img20, "A Band of Digital Warriors", "A band of digital warriors, forging innovation through lines of code, hacking through challenges to unlock a future of endless possibilities"]
   ];
 
   const openModal = (image) => {
     setSelectedImage(image);
     modal.current.style.filter = "blur(4px) brightness(20%)";
   };
-// window.addEventListener('keypress',(e)=>{
-//   if(e.key === "Escape") {
-//     modal.current.style.filter = "none";
-//   }
-// })
+  // window.addEventListener('keypress',(e)=>{
+  //   if(e.key === "Escape") {
+  //     modal.current.style.filter = "none";
+  //   }
+  // })
   const closeModal = () => {
     setSelectedImage([]);
     modal.current.style.filter = "none";
@@ -121,8 +121,10 @@ const Gallery = () => {
             return (
               <button
                 type="button"
-                onClick={() => {openModal(element) 
-                  console.log(element)}}
+                onClick={() => {
+                  openModal(element)
+                  console.log(element)
+                }}
                 className="w-1/3 p-4"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
--- uhackathon23/src/pages/problem statement/Problem.js
+++ uhackathon23/src/pages/problem statement/Problem.js
@@ -1,39 +1,141 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Problem.css";
-import logo from "./3D modelling.png";
-import { color } from "framer-motion";
+import Heading from "../../Components/Heading/Heading";
+
+import data from "./statement.json";
 
 const Problem = () => {
+  const modalRef = useRef(null);
+
+  const openModal = () => {
+    const modalElement = modalRef.current;
+    modalElement.style.display = "flex";
+  };
+
+  const handleCloseModal = () => {
+    const modalElement = modalRef.current;
+    modalElement.style.display = "none";
+  };
+
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
+
+  function psCard(data) {
+    return (
+      <div className="col-4 card-container">
+        <button
+          className="card d-flex flex-row align-items-center justify-content-start p-3 pl-4 gap-4 w-100"
+          onClick={openModal}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="2em"
+            viewBox="0 0 448 512"
+          >
+            <path d="M224 0c13.3 0 24 10.7 24 24V70.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-57 57v76.5l66.2-38.2 20.9-77.8c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4L373 142.2l37.1-21.4c11.5-6.6 26.2-2.7 32.8 8.8s2.7 26.2-8.8 32.8L397 183.8l31.5 8.4c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-77.8-20.9L272 256l66.2 38.2 77.8-20.9c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4L397 328.2l37.1 21.4c11.5 6.6 15.4 21.3 8.8 32.8s-21.3 15.4-32.8 8.8L373 369.8l8.4 31.5c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-20.9-77.8L248 297.6v76.5l57 57c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V441.9l-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57V297.6l-66.2 38.2-20.9 77.8c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4L75 369.8 37.9 391.2c-11.5 6.6-26.2 2.7-32.8-8.8s-2.7-26.2 8.8-32.8L51 328.2l-31.5-8.4c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l77.8 20.9L176 256l-66.2-38.2L31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4L51 183.8 13.9 162.4c-11.5-6.6-15.4-21.3-8.8-32.8s21.3-15.4 32.8-8.8L75 142.2l-8.4-31.5c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l20.9 77.8L200 214.4V137.9L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V24c0-13.3 10.7-24 24-24z" />
+          </svg>
+          <div className="card-content d-flex flex-column align-items-start justify-content-center">
+            <h5
+              style={{ fontWeight: "600", color: "#2E2545", textAlign: "left" }}
+            >
+              {data.title}
+            </h5>
+            <h6 style={{ fontWeight: "600", color: "#8585BD" }}>{data.id}</h6>
+          </div>
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="container problem-statement-card two-margin-top">
-      <div className="row d-fex flex-row flex-wrap">
-        <div className="col-4">
-          <div className="card d-flex flex-row align-items-center justify-content-start p-3 pl-4 gap-4">
-            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M224 0c13.3 0 24 10.7 24 24V70.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-57 57v76.5l66.2-38.2 20.9-77.8c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4L373 142.2l37.1-21.4c11.5-6.6 26.2-2.7 32.8 8.8s2.7 26.2-8.8 32.8L397 183.8l31.5 8.4c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-77.8-20.9L272 256l66.2 38.2 77.8-20.9c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4L397 328.2l37.1 21.4c11.5 6.6 15.4 21.3 8.8 32.8s-21.3 15.4-32.8 8.8L373 369.8l8.4 31.5c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-20.9-77.8L248 297.6v76.5l57 57c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V441.9l-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57V297.6l-66.2 38.2-20.9 77.8c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4L75 369.8 37.9 391.2c-11.5 6.6-26.2 2.7-32.8-8.8s-2.7-26.2 8.8-32.8L51 328.2l-31.5-8.4c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l77.8 20.9L176 256l-66.2-38.2L31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4L51 183.8 13.9 162.4c-11.5-6.6-15.4-21.3-8.8-32.8s21.3-15.4 32.8-8.8L75 142.2l-8.4-31.5c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l20.9 77.8L200 214.4V137.9L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V24c0-13.3 10.7-24 24-24z"/></svg>
-           <div className="card-content d-flex flex-column align-items-start justify-content-center" >
-            <h5 style={{fontWeight:"600" ,color:"#2E2545",textAlign:"left"}}>Analysis of Hinglish Content</h5>
-            <h6 style={{fontWeight:"600",color:"#8585BD"}}>PSAIML001</h6>
-            </div>
-          </div>
+      <div ref={modalRef} className="modal">
+        {/* Modal content */}
+        <div className="modal-content">
+          <span className="close" onClick={handleCloseModal}>
+            &times;
+          </span>
+          <section className="container d-flex align-items-start justify-content-center p-4 flex-column">
+            <h6 className="mb-4 ps-number" style={{ fontWeight: "600" }}>
+              PS NUMBER: PSAIML001
+            </h6>
+            <span className="d-flex flex-row">
+              <h6
+                className="blue-color"
+                style={{ color: "#0043ce", fontWeight: "600" }}
+              >
+                Problem Statement:&nbsp;{" "}
+              </h6>{" "}
+              <h6 className="norm" style={{ fontWeight: "600" }}>
+                Artificial Intelligence
+              </h6>
+            </span>
+            <span className="d-flex flex-row">
+              <h6
+                className="blue-color"
+                style={{ color: "#0043ce", fontWeight: "600" }}
+              >
+                Key:&nbsp;{" "}
+              </h6>{" "}
+              <h6 className="norm" style={{ fontWeight: "600" }}>
+                Lorem
+              </h6>
+            </span>
+            <span className="d-flex flex-row mb-4">
+              <h6
+                className="blue-color"
+                style={{ color: "#0043ce", fontWeight: "600" }}
+              >
+                Organisation:&nbsp;{" "}
+              </h6>{" "}
+              <h6 className="norm" style={{ fontWeight: "600" }}>
+                IIT Bombay
+              </h6>
+            </span>
+            <span style={{ textAlign: "justify" }} className="mb-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                magni at, cupiditate quos consequatur corporis excepturi iste
+                perspiciatis corrupti porro accusamus nulla officiis, ipsam
+                alias sint ex laudantium debitis a? Aspernatur laborum, iure
+                inventore, itaque repellat neque maxime qui quidem, vitae optio
+                quis cumque exercitationem dolore temporibus consequatur.
+                Ducimus sed velit magni, quo saepe cupiditate omnis molestias ad
+                numquam aliquid. Obcaecati facilis voluptatibus quas natus
+                cumque corporis magni delectus qui! Ipsam praesentium ratione
+                exercitationem nemo, quod architecto magnam quibusdam
+                voluptatum, deserunt tempore doloremque minima natus hic harum
+                veniam culpa aspernatur. Ipsa cupiditate ipsum magni,
+                consequatur aliquam blanditiis consectetur, fugiat eos alias
+                reprehenderit assumenda odio quisquam ut rem expedita dolores
+                itaque nemo ratione eum nam aperiam. Consequatur quisquam ad
+                iste ab!0
+              </p>
+            </span>
+            <form action="" method="post" style={{display:"grid",placeItems:"flex-start"}} >
+            <div class="input-grou mb-3">
+    <label class="label">Team Name</label>
+    <input autocomplete="off" name="Email" id="Email" class="input" type="text"/>
+    <div></div></div>
+    <div class="input-grou mb-4">
+    <label class="label">Team ID</label>
+    <input autocomplete="off" name="Email" id="Email" class="input" type="text"/>
+    <div></div></div>
+<button type="submit" className="join-btn p-2">Submit</button>
+            </form>
+          </section>
         </div>
-        <div className="col-4">
-          <div className="card d-flex flex-row align-items-center justify-content-start p-3 pl-4 gap-4">
-            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M224 0c13.3 0 24 10.7 24 24V70.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-57 57v76.5l66.2-38.2 20.9-77.8c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4L373 142.2l37.1-21.4c11.5-6.6 26.2-2.7 32.8 8.8s2.7 26.2-8.8 32.8L397 183.8l31.5 8.4c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-77.8-20.9L272 256l66.2 38.2 77.8-20.9c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4L397 328.2l37.1 21.4c11.5 6.6 15.4 21.3 8.8 32.8s-21.3 15.4-32.8 8.8L373 369.8l8.4 31.5c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-20.9-77.8L248 297.6v76.5l57 57c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V441.9l-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57V297.6l-66.2 38.2-20.9 77.8c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4L75 369.8 37.9 391.2c-11.5 6.6-26.2 2.7-32.8-8.8s-2.7-26.2 8.8-32.8L51 328.2l-31.5-8.4c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l77.8 20.9L176 256l-66.2-38.2L31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4L51 183.8 13.9 162.4c-11.5-6.6-15.4-21.3-8.8-32.8s21.3-15.4 32.8-8.8L75 142.2l-8.4-31.5c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l20.9 77.8L200 214.4V137.9L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V24c0-13.3 10.7-24 24-24z"/></svg>
-           <div className="card-content d-flex flex-column align-items-start justify-content-center" >
-            <h5 style={{fontWeight:"600" ,color:"#2E2545",textAlign:"left"}}>Analysis of Hinglish Content</h5>
-            <h6 style={{fontWeight:"600",color:"#8585BD"}}>PSAIML001</h6>
-            </div>
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="card d-flex flex-row align-items-center justify-content-start p-3 pl-4 gap-4">
-            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M224 0c13.3 0 24 10.7 24 24V70.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-57 57v76.5l66.2-38.2 20.9-77.8c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4L373 142.2l37.1-21.4c11.5-6.6 26.2-2.7 32.8 8.8s2.7 26.2-8.8 32.8L397 183.8l31.5 8.4c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-77.8-20.9L272 256l66.2 38.2 77.8-20.9c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4L397 328.2l37.1 21.4c11.5 6.6 15.4 21.3 8.8 32.8s-21.3 15.4-32.8 8.8L373 369.8l8.4 31.5c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-20.9-77.8L248 297.6v76.5l57 57c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V441.9l-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57V297.6l-66.2 38.2-20.9 77.8c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4L75 369.8 37.9 391.2c-11.5 6.6-26.2 2.7-32.8-8.8s-2.7-26.2 8.8-32.8L51 328.2l-31.5-8.4c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l77.8 20.9L176 256l-66.2-38.2L31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4L51 183.8 13.9 162.4c-11.5-6.6-15.4-21.3-8.8-32.8s21.3-15.4 32.8-8.8L75 142.2l-8.4-31.5c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l20.9 77.8L200 214.4V137.9L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V24c0-13.3 10.7-24 24-24z"/></svg>
-           <div className="card-content d-flex flex-column align-items-start justify-content-center" >
-            <h5 style={{fontWeight:"600" ,color:"#2E2545",textAlign:"left"}}>Analysis of Hinglish Content</h5>
-            <h6 style={{fontWeight:"600",color:"#8585BD"}}>PSAIML001</h6>
-            </div>
-          </div>
-        </div>
+      </div>
+
+      <Heading text="Problem Statements" position="center"></Heading>
+
+      <div className="d-flex flex-row ps-box">
+        {data.map((element) => {
+          return psCard(element);
+        })}
       </div>
     </div>
   );
