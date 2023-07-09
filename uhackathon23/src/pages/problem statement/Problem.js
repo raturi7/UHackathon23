22c5267b837bc83c61dd0a2f5bc0de47259daf7d
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-07-09 15:42:59 +0530

    fix code

--- uhackathon23/src/pages/problem statement/Problem.js
+++ uhackathon23/src/pages/problem statement/Problem.js
@@ -5,10 +5,13 @@ import Heading from "../../Components/Heading/Heading";
 import data from "./statement.json";
 
 const Problem = () => {
-  const {ps,setPs}=useState({})
+
+  const [ps,setPs]=useState({});
   const modalRef = useRef(null);
 
-  const openModal = () => {
+
+
+  const openModal =  () => {
     const modalElement = modalRef.current;
     modalElement.style.display = "flex";
   };
@@ -29,9 +32,9 @@ const Problem = () => {
       <div className="col-4 card-container">
         <button
           className="card d-flex flex-row align-items-center justify-content-start p-3 pl-4 gap-4 w-100"
-          onClick={()=>{
-            
-            openModal()
+          onClick={async ()=>{
+           setPs(data)
+            openModal(data)
           }}
         >
           <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>
@@ -56,65 +59,20 @@ const Problem = () => {
           <span className="close" onClick={handleCloseModal}>
             &times;
           </span>
-          <section className="d-flex align-items-start justify-content-center p-4 flex-column" style={{textAlign:"left"}}>
+   
+<section className="d-flex align-items-start justify-content-center p-4 flex-column" style={{textAlign:"left"}}>
             <h6 className="mb-4 ps-number" style={{ fontWeight: "600" }}>
-              PS NUMBER: PSAIML001
+              {"PS NUMBER: "+ps.id}
             </h6>
-            <p style={{marginBottom:"8px"}} className="blue-title"><b style={{color:"#0043ce"}}>Problem Statement:</b> <b>Analysis of Hinglish Content</b></p>
-            <p style={{marginBottom:"8px"}}  className="blue-title"><b style={{color:"#0043ce"}}>Key:</b> <b>Lorem</b></p>
-            <p style={{marginBottom:"8px"}} className="mb-4 blue-title"><b style={{color:"#0043ce"}}>Organisation:</b> <b>IIT Bombay</b></p>
+            <p style={{marginBottom:"8px"}} className="blue-title"><b style={{color:"#0043ce"}}>Problem Statement:</b> <b>{ps.title}</b></p>
+            <p style={{marginBottom:"8px"}}  className="blue-title"><b style={{color:"#0043ce"}}>Key:</b> <b>{ps.key}</b></p>
+            <p style={{marginBottom:"8px"}} className="mb-4 blue-title"><b style={{color:"#0043ce"}}>Organisation:</b> <b>{ps.organisation}</b></p>
             <span style={{ textAlign: "justify" }} className="mb-4">
               <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                magni at, cupiditate quos consequatur corporis excepturi iste
-                perspiciatis corrupti porro accusamus nulla officiis, ipsam
-                alias sint ex laudantium debitis a? Aspernatur laborum, iure
-                inventore, itaque repellat neque maxime qui quidem, vitae optio
-                quis cumque exercitationem dolore temporibus consequatur.
-                Ducimus sed velit magni, quo saepe cupiditate omnis molestias ad
-                numquam aliquid. Obcaecati facilis voluptatibus quas natus
-                cumque corporis magni delectus qui! Ipsam praesentium ratione
-                exercitationem nemo, quod architecto magnam quibusdam
-                voluptatum, deserunt tempore doloremque minima natus hic harum
-                veniam culpa aspernatur. Ipsa cupiditate ipsum magni,
-                consequatur aliquam blanditiis consectetur, fugiat eos alias
-                reprehenderit assumenda odio quisquam ut rem expedita dolores
-                itaque nemo ratione eum nam aperiam. Consequatur quisquam ad
-                iste ab!0
+                {ps.Statement}
               </p>
             </span>
-            {/* <form
-              action=""
-              method="post"
-              style={{ display: "grid", placeItems: "flex-start" }}
-            >
-              <div class="input-grou mb-3">
-                <label class="label">Team Name</label>
-                <input
-                  autocomplete="off"
-                  name="Email"
-                  id="Email"
-                  class="input"
-                  type="text"
-                />
-                <div></div>
-              </div>
-              <div class="input-grou mb-4">
-                <label class="label">Team ID</label>
-                <input
-                  autocomplete="off"
-                  name="Email"
-                  id="Email"
-                  class="input"
-                  type="text"
-                />
-                <div></div>
-              </div>
-              <button type="submit" className="join-btn p-2">
-                Submit
-              </button>
-            </form> */}
-          </section>
+          </section> 
         </div>
       </div>
 
