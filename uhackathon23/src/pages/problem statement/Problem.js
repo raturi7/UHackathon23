84e50f9aa0f80cbbe29b26c2765f6e66e0b7b5ec
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-07-09 19:57:52 +0530

    add json

--- uhackathon23/src/pages/problem statement/Problem.js
+++ uhackathon23/src/pages/problem statement/Problem.js
@@ -6,6 +6,7 @@ import data from "./statement.json";
 
 const Problem = () => {
 
+
   const [ps,setPs]=useState({});
   const modalRef = useRef(null);
  
@@ -16,7 +17,7 @@ const Problem = () => {
     let i = 0;
     let htmlData = [];
     for (i = 0; i < keys.length; i++) {
-      const htmlString = `<p style="marginBottom: 8px;" className="blue-title"><b style="color: #0043ce;">${keys[i]}:</b> <b>${values[i]}</b></p>`;
+      const htmlString = `<p style="marginBottom: 8px;text-align:justify" className="blue-title"><b style="color: #0043ce;">${keys[i]}:</b> <b>${values[i]}</b></p>`;
       htmlData.push(React.createElement('div', { dangerouslySetInnerHTML: { __html: htmlString } }));
     }
     return htmlData;
@@ -61,13 +62,15 @@ const Problem = () => {
           }}
         >
           <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>
-          <div className="card-content d-flex flex-column align-items-start justify-content-center title-ps"  > 
+          <div className="card-content d-flex flex-column align-items-start justify-content-center gap-2" > 
+          <div className="title-ps">
             <h5
-              style={{ fontWeight: "600", color: "#2E2545", textAlign: "left",maxHeight:"40px"}} 
+              style={{ fontWeight: "600", color: "#2E2545", textAlign: "left",maxHeight:"40px",marginBottom:"0px"}} 
             >
               {data.Title}
             </h5>
-            <h6 style={{ fontWeight: "600", color: "#8585BD" }}>{data.id}</h6>
+            </div>
+            <h6 style={{ fontWeight: "600", color: "#8585BD" }}>{"UHACK01"}</h6>
           </div>
         </button>
       </div>
@@ -88,14 +91,6 @@ const Problem = () => {
               {"PS NUMBER: "+ps.id}
             </h6>
             {printData()}
-           
-            {/* <p style={{marginBottom:"8px"}}  className="blue-title"><b style={{color:"#0043ce"}}>Key:</b> <b>{ps.key}</b></p>
-            <p style={{marginBottom:"8px"}} className="mb-4 blue-title"><b style={{color:"#0043ce"}}>Organisation:</b> <b>{ps.organisation}</b></p> */}
-            <span style={{ textAlign: "justify" }} className="mb-4">
-              <p className="description">
-                {ps.Statement}
-              </p>
-            </span>
           </section> 
         </div>
       </div>
