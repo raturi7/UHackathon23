f5e5973a4c4ee62d7fef6956b3b755a9a26b8a46
Raghav-upes <116428068+Raghav-upes@users.noreply.github.com>
2023-07-09 18:31:33 +0530

    fix all the changes

--- uhackathon23/src/pages/problem statement/Problem.js
+++ uhackathon23/src/pages/problem statement/Problem.js
@@ -12,9 +12,25 @@ const Problem = () => {
   const [keys,setKeys]=useState([])
   const [values,setValues]=useState([])
 
-  function printData(){
-    
+  function printData() {
+    let i = 0;
+    let htmlData = [];
+    for (i = 0; i < keys.length; i++) {
+      const htmlString = `<p style="marginBottom: 8px;" className="blue-title"><b style="color: #0043ce;">${keys[i]}:</b> <b>${values[i]}</b></p>`;
+      htmlData.push(React.createElement('div', { dangerouslySetInnerHTML: { __html: htmlString } }));
+    }
+    return htmlData;
   }
+  
+//   function printData(){
+//     let i=0;
+//     let data=""
+//     for(i=0;i<keys.length;i++)
+// {
+//    data+=`<p style={{marginBottom:"8px"}} className="blue-title"><b style={{color:"#0043ce"}}>${keys[i]}</b> <b>${values[i]}</b></p>`
+// }
+// return data;
+//   }
 
   const openModal =  () => {
     const modalElement = modalRef.current;
@@ -45,11 +61,11 @@ const Problem = () => {
           }}
         >
           <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>
-          <div className="card-content d-flex flex-column align-items-start justify-content-center">
+          <div className="card-content d-flex flex-column align-items-start justify-content-center title-ps"  > 
             <h5
-              style={{ fontWeight: "600", color: "#2E2545", textAlign: "left" }}
+              style={{ fontWeight: "600", color: "#2E2545", textAlign: "left",maxHeight:"40px"}} 
             >
-              {data.title}
+              {data.Title}
             </h5>
             <h6 style={{ fontWeight: "600", color: "#8585BD" }}>{data.id}</h6>
           </div>
@@ -71,10 +87,10 @@ const Problem = () => {
             <h6 className="mb-4 ps-number" style={{ fontWeight: "600" }}>
               {"PS NUMBER: "+ps.id}
             </h6>
-            {}
-            <p style={{marginBottom:"8px"}} className="blue-title"><b style={{color:"#0043ce"}}>Problem Statement:</b> <b>{ps.title}</b></p>
-            <p style={{marginBottom:"8px"}}  className="blue-title"><b style={{color:"#0043ce"}}>Key:</b> <b>{ps.key}</b></p>
-            <p style={{marginBottom:"8px"}} className="mb-4 blue-title"><b style={{color:"#0043ce"}}>Organisation:</b> <b>{ps.organisation}</b></p>
+            {printData()}
+           
+            {/* <p style={{marginBottom:"8px"}}  className="blue-title"><b style={{color:"#0043ce"}}>Key:</b> <b>{ps.key}</b></p>
+            <p style={{marginBottom:"8px"}} className="mb-4 blue-title"><b style={{color:"#0043ce"}}>Organisation:</b> <b>{ps.organisation}</b></p> */}
             <span style={{ textAlign: "justify" }} className="mb-4">
               <p className="description">
                 {ps.Statement}
