3ad631b8855d715de55dc75d2c073b780cb79c20
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-07-24 19:03:03 +0530

    collabarotrs

--- uhackathon23/src/Components/Our-Colaborators/OurCollaborators.js
+++ uhackathon23/src/Components/Our-Colaborators/OurCollaborators.js
@@ -1,16 +1,16 @@
 import React from "react";
-import img1 from "./Images/socs.png";
-import img2 from "./Images/Moretech Solutions SA.png";
-import img3 from "./Images/good good piggy.png";
+import img1 from "./Images/SOCS UPES.png";
+import img2 from "./Images/Moretech Solutions Pvt. Ltd..png";
+import img3 from "./Images/Good Good Piggy.png";
 import img4 from "./Images/India Blockchain Alliance.png";
 import img5 from "./Images/Cognecto.png";
-import img6 from "./Images/XR COTURE.png";
-import img7 from "./Images/c4i4.png";
-import img8 from "./Images/Flugel Soft.png";
-import img9 from "./Images/Kosha Foods.png";
-import img10 from "./Images/Divin Pro.png";
-import img11 from "./Images/HiTech Saw.png";
-import img12 from "./Images/MarshMcLennan.png";
+import img6 from "./Images/XR Couture.png";
+import img7 from "./Images/C4i4 labs.png";
+import img8 from "./Images/Flugel SOFT.png";
+import img9 from "./Images/Koshaa Foods.png";
+import img10 from "./Images/DivIn Pro.png";
+import img11 from "./Images/Hitech Saw.png";
+import img12 from "./Images/Marsh McLennan.png";
 import img13 from "./Images/Soran Kids Foundation.png";
 import Heading from "../Heading/Heading";
 import "slick-carousel/slick/slick.css";
@@ -21,7 +21,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-const imgaes = [img1];
+const imgaes = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11,img12,img13];
 function sponsorCard(img) {
   return (
     <div class="card p-4">
@@ -53,13 +53,13 @@ const OurCollaborators = () => {
   return (
     <>
       <div className="container d-flex align-items-center justify-content-center flex-row flex-wrap sponsor-container two-margin-top pc-container">
-        <Heading text="Sponsors" position="center" />
+        <Heading text="Our Collaborators" position="center" />
         {imgaes.map((element) => {
           return sponsorCard(element);
         })}
       </div>
       <div className="container mobile-container sponsor-container mobile-margin">
-        <Heading text="Sponsors" position="center" />
+        <Heading text="Our Collaborators" position="center" />
         <div className="container">
           <Slider {...settings}>
             {imgaes.map((element) => {
